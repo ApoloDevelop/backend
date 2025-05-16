bfb61699bd0578525ad62d60b088f68920ab3977
Pablo Robles <pablormans@gmail.com>
2025-05-16 14:06:40 +0200

    implemented cloudinary logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { CloudinaryController } from './cloudinary/cloudinary.controller';
+import { CloudinaryService } from './cloudinary/cloudinary.service';
 
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
+  controllers: [CloudinaryController],
+  providers: [CloudinaryService],
 })
 export class AppModule {}
